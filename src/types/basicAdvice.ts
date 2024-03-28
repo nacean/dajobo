0f8bd8cf3f672c710cdb82d6eb34eda89482ffcb
nacean <nacean@naver.com>
2024-03-28 22:15:44 +0900

    feat: 기본 조언 csv 및 타입 추가

---
+++ src/types/basicAdvice.ts
@@ -0,0 +1,26 @@
+export interface basicAdvice {
+  adviceNum: number;
+  explain: string;
+  target: number;
+  upTarget: number;
+  downTarget: number;
+  upGauge: number;
+  downGauge: number;
+  probability: number;
+  //notAppear : number[] ?
+  weight0: number;
+  weight1: number;
+  weight2: number;
+  weight3: number;
+  weight4: number;
+  weight5: number;
+  weight6: number;
+  weight7: number;
+  weight8: number;
+  weight9: number;
+  weight10: number;
+  weight11: number;
+  weight12: number;
+  weight13: number;
+  weight14: number;
+}
